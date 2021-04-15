10f60265cb4c4216870560116a8ebd79dec3cb9f
kevAs <kevassiobo@gmail.com>
2021-04-15 13:32:48 +0200

    base

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+let ma_phrase = "j'aime les tartines et j'en mange";
+console.log(ma_phrase);
+let nombre_par_jour = "4 par jour ";
+console.log(nombre_par_jour);
+console.log(ma_phrase + nombre_par_jour );
+let troiseme = 5;
+let quartrieme = 35;
+console.log( ma_phrase + " " + troiseme);
+
+let ma_premiere_phrase ="salut tout le monde";
+console.log(ma_premiere_phrase);
+let  ma_deuxieme_phrase = "Aurevoir";
+console.log(  ma_deuxieme_phrase );
+ma_premiere_phrase = ma_deuxieme_phrase
+console.log(ma_premiere_phrase);
+
+
+
+let numbre1 = 4;
+let numbre2 = 6;
+let resultat1 = 4 > 6;
+console.log( resultat1);
+let resultat2 = 4 < 6;
+console.log( resultat2);
+let resultat3 = 4 == 6;
+console.log(resultat3);
+let resultat4 = 4 != 6;
+console.log( resultat4);
+let resultat5 = 4 === 6;
+console.log( resultat5);
+let resultat6 = 4 !== 6;
+console.log( resultat6);
